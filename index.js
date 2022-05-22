4fca802a478714471d9982008cb5009a59f159c9
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-05-22 11:46:33 +0600

    data find

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euupn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://car-service:ti2GjpaxIhmF7vZ5@cluster0.euupn.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        await client.connect()
+        const CartsCollection = client.db('Car_Parts').collection('Parts')
 
+        app.get('/part', async (req, res) => {
+            const query = {}
+            const cursor = CartsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        });
 
     }
     finally {
@@ -29,7 +37,7 @@ run().catch(console.dir())
 
 
 app.get('/', (req, res) => {
-    res.send('assignment 12 is fire')
+    res.send('assignment 12 is on fire')
 })
 
 app.listen(port, () => {
